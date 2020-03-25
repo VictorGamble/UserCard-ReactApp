87c45dc463b1fa6d524229f6dfbfa0b541b28336
Victor <Victor.Gamble@outlook.com>
2020-03-25 15:21:53 -0400

    pulled the firstname and picture from the api

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,15 +3,17 @@ import React, { Component } from 'react';
 
 class Card extends Component{
     state = {
-        quote: "loading"
+        quote: "loading",
+        pic:"pic"
     }
     async componentDidMount() {
         try {
-          const response = await fetch(`https://randomuser.me/`);
+          const response = await fetch(`https://randomuser.me/api/?results=1`);
           const data = await response.json();
-          console.log('data is', data);
+          console.log('data is=>', data);
           this.setState({
-            quote: data.value
+              quote: data.results[0].name.first,
+              pic: data.results[0].picture.large
           })
         } catch (error) {
           console.error("Error", error);
@@ -20,9 +22,11 @@ class Card extends Component{
     }
     render() {
         const { quote } = this.state
+        const {pic} = this.state
         return (
             <div>
                 <div>
+                    <img src={pic} alt="pic"></img>
                     <p>{quote}</p>
                 </div>
             </div>
