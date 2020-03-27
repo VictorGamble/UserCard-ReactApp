bcaeabe858716fb1cccf59ed589483e5a087fbf5
Victor <Victor.Gamble@outlook.com>
2020-03-27 13:24:00 -0400

    refactored app.js into a class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import Card from './components/card'
+import React, {Component} from 'react';
+import ProCard from './components/ProCard';
 import 'bulma/css/bulma.css'
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Card/>
-    </div>
-  );
+
+
+class App extends Component{
+  
+  state = {
+    userData: [],
+    title: "Fetching Data"
+  }
+  
+  dataLoad = async () => {
+    try {
+      const response = await fetch(`https://randomuser.me/api/?results=3`)
+      const data = await response.json()
+      return data.results 
+    } catch (error) {
+      console.error("Error", error);
+      return error;
+    }
+  }
+  
+  clickHandler = async () => {
+    const newData = await this.dataLoad();
+    this.setState({
+      userData: newData
+    })
+  }
+  
+
+  async componentDidMount() {
+    const userData = await this.dataLoad();
+    this.setState({
+      userData : userData
+    })
+  }
+  
+  render() {
+    const { title, userData } = this.state;
+    return (
+      <div className="App">
+        <h1>{title}</h1>
+        <ProCard userData={userData} />
+        <button onClick={this.clickHandler}>New Users</button>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ProCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+
+const ProCard = props => {
+  const { user } = props;
+  console.log("User is", user)
+  return (
+ null
+  )
+}
+
+
+
+
+export default ProCard;
