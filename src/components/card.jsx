0367c2cc2d260c5d223da65456acc35d97615570
Victor <Victor.Gamble@outlook.com>
2020-03-27 11:37:32 -0400

    getting the data to look more like a card, using bulma

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,61 +1,51 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Card, CardImage, CardContent, Column, Columns } from "bloomer";
+class UserCard extends Component {
+  state = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    pic: "pic",
+    email: "email",
+    phone: "phone"
+  };
 
-class UserCard extends Component{
-    state = {
-        firstName: "First Name",
-        lastName: "Last Name",
-        pic: "pic",
-        email: "email",
-        phone: "phone"
-        
+  async componentDidMount() {
+    try {
+      const response = await fetch(`https://randomuser.me/api/?results=1`);
+      const data = await response.json();
+      console.log("data is=>", data);
+      this.setState({
+        firstName: data.results[0].name.first,
+        lastName: data.results[0].name.last,
+        pic: data.results[0].picture.thumbnail,
+        email: data.results[0].email,
+        phone: data.results[0].phone
+      });
+    } catch (error) {
+      console.error("Error", error);
+      return error;
     }
-    async componentDidMount() {
-        try {
-          const response = await fetch(`https://randomuser.me/api/?results=1`);
-          const data = await response.json();
-          console.log('data is=>', data);
-          this.setState({
-              firstName: data.results[0].name.first,
-              lastName: data.results[0].name.last,
-              pic: data.results[0].picture.large,
-              email: data.results[0].email,
-              phone: data.results[0].phone,
-
-          })
-        } catch (error) {
-          console.error("Error", error);
-          return error;
-        }
-    }
-    render() {
-        const { firstName } = this.state
-        const { pic } = this.state
-        const { lastName } = this.state
-        const { email } = this.state
-        const {phone} = this.state
-        return (
-            <div>
-
-<Card style={{ width: '18rem'}}>
-  <Card.Img variant="top" src={pic} />
-  <Card.Body>
-    <Card.Title>User</Card.Title>
-    <Card.Text>
-      {firstName}, 
-      {lastName}
-                            <br></br>
-                            {email}
-                            <br></br>                
-         {phone}
-     </Card.Text>
-  </Card.Body>
-</Card>
-            </div>
-        )
-    }
+  }
+  render() {
+    const { firstName } = this.state;
+    const { pic } = this.state;
+    const { lastName } = this.state;
+    const { email } = this.state;
+    const { phone } = this.state;
+    return (
+      <div>
+        <Column>
+          <Card>
+            <CardImage image src={pic}></CardImage>
+            <CardContent>{firstName}</CardContent>
+            <CardContent>{lastName}</CardContent>
+            <CardContent>{email}</CardContent>
+            <CardContent>{phone}</CardContent>
+          </Card>
+        </Column>
+      </div>
+    );
+  }
 }
-
 
 export default UserCard;
